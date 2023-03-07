813f10fbd0bbbb501a7d7e9c13dc83b6f62cedfd
phvietanh1998 <58402497+phvietanh1998@users.noreply.github.com>
2023-03-07 19:57:53 +0300

    Update config.js

--- config.js
+++ config.js
@@ -3,7 +3,7 @@ const giftUrl = "https://www.instagram.com/sullicious_/";
 const eventName = "CHÚC MỪNG 8/3";
 const titleCard = "To Sullicious";
 const contentCard =
-  "Em iu! Từ khi em bước đến bên anh là cuộc sống của anh luôn rực rỡ và ngập tràn hạnh phúc. Cảm ơn em vì tất cả. Dịp 8 tháng 3 này anh không ở nhà để đưa em đi chơi được nhưng em cũng đừng buồn nhé bởi con tim, lý trí của anh lúc nào cũng hướng trọn về em. Yêu em pé của anh! From pva98 with <3";
+  "Em iu! Từ khi em bước đến bên anh là cuộc sống của anh luôn rực rỡ và ngập tràn hạnh phúc. Cảm ơn em vì tất cả. Dịp 8 tháng 3 này anh không ở nhà để đưa em đi chơi được nhưng em cũng đừng buồn nhé bởi con tim, lý trí của anh lúc nào cũng hướng trọn về em. Yêu em pé đáng iu chinh đẹp nhất trên đời của anh! From pva98 with <3";
 
 // phần dưới dành cho các bạn biết code, nếu muốn chỉnh ảnh đơn giản với base64
 // Cần hỗ trợ hãy liên hệ:

6a26f1774ae23101741f32e3d1e4e2fee8bb719c
phvietanh1998 <58402497+phvietanh1998@users.noreply.github.com>
2023-03-07 23:55:21 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,14 @@
+const nameGirl = "sullicious_";
+const giftUrl = "https://www.instagram.com/sullicious_/";
+const eventName = "CHÚC MỪNG 8/3";
+const titleCard = "To Sullicious";
+const contentCard =
+  "Em iu! Từ khi em bước đến bên anh là cuộc sống của anh luôn rực rỡ và ngập tràn hạnh phúc. Cảm ơn em vì tất cả. Dịp 8 tháng 3 này anh không ở nhà để đưa em đi chơi được nhưng em cũng đừng buồn nhé bởi con tim, lý trí của anh lúc nào cũng hướng trọn về em. Yêu em pé của anh! From pva98 with <3";
+
+// phần dưới dành cho các bạn biết code, nếu muốn chỉnh ảnh đơn giản với base64
+// Cần hỗ trợ hãy liên hệ:
+// Mr-Nam http://facebook.com/nam.nodemy
+// Các bạn muốn học lập trình thì tham gia Nhóm zalo tự học lập trình nhé: https://zalo.me/g/yhdkef092
+const giftImage = "sullicious.JPG";
+const base64 = "";
+const giftImageBase64 = "data:image/png;base64, " + base64;
